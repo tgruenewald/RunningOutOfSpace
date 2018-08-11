402e0872ee53537c3e1e23a12d09cd39eb464398
tgruenewald <terrygruenewald@gmail.com>
2018-08-11 10:44:44 -0600

    better movement

--- RunningOutOfSpace/Assets/Scripts/Player.cs
+++ RunningOutOfSpace/Assets/Scripts/Player.cs
@@ -179,21 +179,7 @@ public class Player : MonoBehaviour {
 			}
 		}     
 
-    //  void FixedUpdate () {
-    //      if(Input.GetKey(KeyCode.A) && transform.position == pos) {        // Left
-    //          pos += Vector3.left;
-    //      }
-    //      if(Input.GetKey(KeyCode.D) && transform.position == pos) {        // Right
-    //          pos += Vector3.right;
-    //      }
-    //      if(Input.GetKey(KeyCode.W) && transform.position == pos) {        // Up
-    //          pos += Vector3.up;
-    //      }
-    //      if(Input.GetKey(KeyCode.S) && transform.position == pos) {        // Down
-    //          pos += Vector3.down;
-    //      }
-    //      transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
-    //  }
+
     void Flip()
     {
         //Debug.Log("switching...");
