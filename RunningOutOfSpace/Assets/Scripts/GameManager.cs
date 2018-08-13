8bd988381b32d6b058f73e5ad57626ccc09791f5
tgruenewald <terrygruenewald@gmail.com>
2018-08-12 22:18:56 -0600

    flooding can start

--- RunningOutOfSpace/Assets/Scripts/GameManager.cs
+++ RunningOutOfSpace/Assets/Scripts/GameManager.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour {
-    public int time = 60;
+    public int time = 5;
     public int wood;
     public int Max_Hay = 100;
     public int Hayamount;
-    public int TimeScale = 5;
+    public int TimeScale = 1; // 5
 
     // Use this for initialization
     void Start () {
@@ -44,11 +44,43 @@ public class GameManager : MonoBehaviour {
     {
         return (time);
     }
+    IEnumerator FloodTime() {
+        yield return new WaitForSeconds(5f);
+        GameObject.Find("close_door").transform.GetChild(0).GetComponent<TileContent>().cellContent =  GameObject.Find("starting_cell");;
+        GameObject.Find("ramp").GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("earth3").GetComponent<SpriteRenderer>().enabled = false;        
+    }
     IEnumerator TimePass()
     {
+        GameObject text = GameObject.Find("FloodTime");
+        while (time > 0) {
+            yield return new WaitForSeconds(TimeScale);
+            text.GetComponent<Text>().text = "Days Remaining Until Flood " + time;
+            time--;            
+
+        }
         
-        yield return new WaitForSeconds(TimeScale);
-        time--;
-        StartCoroutine(TimePass());
+        // flood starts
+
+        // rename starting_cell
+        // rename starting_cell_rain to starting_cell
+        GameObject oldStart = GameObject.Find("starting_cell_rain");
+        GameObject currentStart = GameObject.Find("starting_cell");
+        currentStart.name = "old";
+        oldStart.name = "starting_cell";
+        Player player = GameState.GetPlayerDroplet();
+        SpawnPoint.SwitchToLevel(player.gameObject);
+
+        StartCoroutine(FloodTime());
+
+        // find ramp make invinsible
+
+        // find earth3 "    "
+        // close_door delete
+
+
+
+
+        // StartCoroutine(TimePass());
     }
 }
--- RunningOutOfSpace/Assets/Scripts/TimeText.cs
+++ RunningOutOfSpace/Assets/Scripts/TimeText.cs
@@ -7,12 +7,12 @@ public class TimeText : MonoBehaviour {
     public GameObject dang;
     // Use this for initialization
     void Start () {
-        m_TimeText.text = "hi";
+        // m_TimeText.text = "hi";
     }
 	
 	// Update is called once per frame
 	void Update () {
-        string Jay = "Days Remaining Until Flood" + " " + dang.GetComponent<GameManager>().Timeleft().ToString();
-        m_TimeText.text = Jay;
+        // string Jay = "Days Remaining Until Flood" + " " + dang.GetComponent<GameManager>().Timeleft().ToString();
+        // m_TimeText.text = Jay;
     }
 }
